f368ba213b2a7c1c659ed41a1de1519d26b1fb44
Jose Manuel Gama <josegamaes@gmail.com>
2018-01-07 02:52:31 -0600

    Realizando la configuracion desde un archivo externo.

--- online.py
+++ online.py
@@ -1,29 +1,57 @@
-from selenium import webdriver
+"""
+importing the libraries that we are going to use
+for loadig the settings file and scraping the website
+"""
+import ConfigParser
 import time
 
-# Assigning global variables
-FIREFOX_PATH = "/home/gama/.mozilla/firefox/kpnk7t0r.default"
-NAME = 'Gian'
-PAGE = "https://web.whatsapp.com/"
+from selenium import webdriver
 
-# Function that search the specified user and activates his chat
-def search_chatter(driver):
+
+def asign_settings():
+    """
+    Loading and assigning global variables from our settings.txt file
+    """
+    config_parser = ConfigParser.RawConfigParser()
+    config_file_path = 'settings.txt'
+    config_parser.read(config_file_path)
+
+    firefox_path = config_parser.get('your-config', 'FIREFOX_PATH')
+    name = config_parser.get('your-config', 'NAME')
+    page = config_parser.get('your-config', 'PAGE')
+
+    settings = {'firefox_path': firefox_path, 'name': name, 'page': page}
+    return settings
+
+
+def search_chatter(driver, settings):
+    """
+    Function that search the specified user and activates his chat
+    """
+
     while True:
         for chatter in driver.find_elements_by_xpath("//div[@class='_2wP_Y']"):
-            chatter_name = chatter.find_element_by_xpath(".//span[@class='emojitext ellipsify']").text
-            if chatter_name == NAME:
-                chatter.find_element_by_xpath(".//div[@class='chat-drag-cover']").click()
+            chatter_name = chatter.find_element_by_xpath(
+                ".//span[@class='emojitext ellipsify']").text
+            if chatter_name == settings['name']:
+                chatter.find_element_by_xpath(
+                    ".//div[@class='chat-drag-cover']").click()
                 return
 
-# Loading all the configuration and opening the website (Firefox profile where whatsapp web is already scanned)
 
-firefox_profile = webdriver.FirefoxProfile(FIREFOX_PATH)
-driver = webdriver.Firefox(firefox_profile)
-driver.get(PAGE)
+"""
+Loading all the configuration and opening the website
+(Firefox profile where whatsapp web is already scanned)
+"""
+SETTINGS = asign_settings()
+FIREFOX_PROFILE = webdriver.FirefoxProfile(SETTINGS['firefox_path'])
+DRIVER = webdriver.Firefox(FIREFOX_PROFILE)
+DRIVER.get(SETTINGS['page'])
 
-search_chatter(driver)
-
-# Waiting 10sec before closing the navigator
+search_chatter(DRIVER, SETTINGS)
+"""
+Waiting 10 sec before closing the navigator
+"""
 time.sleep(10)
 
-driver.close()
+DRIVER.close()
